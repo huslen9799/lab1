de3dd3bf142386438e2b64d70cd92b8b08ae8341
huslen9799 <sw23d024@mandakh.edu.mn>
2025-09-09 16:41:10 +0800

    Revert "header"
    This reverts commit 47b1fb095fbbb68e69e599acf71fba4975c6e896.

--- myapp/src/app/page.tsx
+++ myapp/src/app/page.tsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
+import Image from "next/image";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+export default function Home() {
   return (
-    <div className="p-4">
-      <p>Тоолуур: {count}</p>
-      <button 
-        className="px-4 py-2 bg-green-500 text-white rounded"
-        onClick={() => setCount(count + 1)}>
-        Нэмэх
-      </button>
+    <div >
+     <p>LABi</p>
     </div>
   );
 }
