47b1fb095fbbb68e69e599acf71fba4975c6e896
huslen9799 <sw23d024@mandakh.edu.mn>
2025-09-09 15:07:20 +0800

    header

--- myapp/src/app/page.tsx
+++ myapp/src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { useState } from "react";
 
-export default function Home() {
+export default function Counter() {
+  const [count, setCount] = useState(0);
   return (
-    <div >
-     <p>LABi</p>
+    <div className="p-4">
+      <p>Тоолуур: {count}</p>
+      <button 
+        className="px-4 py-2 bg-green-500 text-white rounded"
+        onClick={() => setCount(count + 1)}>
+        Нэмэх
+      </button>
     </div>
   );
 }
