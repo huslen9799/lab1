e7ae0475653c5ca716688a88f757dedbc1f7061a
huslen9799 <sw23d024@mandakh.edu.mn>
2025-09-16 16:10:41 +0800

    AS

--- myapp/src/components/Layout.tsx
+++ myapp/src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
+      <Navbar />
       <main className="flex-grow container mx-auto p-6">{children}</main>
       <Footer />
     </div>
