a1aea446fa5fff0b1097b55794d9277fcb4834a4
huslen9799 <sw23d024@mandakh.edu.mn>
2025-11-25 16:12:34 +0800

    af

--- myapp/src/components/Navbar.tsx
+++ myapp/src/components/Navbar.tsx
@@ -14,8 +14,8 @@ export default function Navbar() {
       <Link href="/counter/">Counter</Link>
       <Link href="/todo/">Todo</Link>
       <Link href="/localstorage/">storage</Link>
-      <Link href="/form/register/">Register</Link>
-      <Link href="/form/login/">Login</Link>
+      <Link href="/form/register1/">Register</Link>
+      <Link href="/form/login1/">Login</Link>
     </nav>
   );
 }
